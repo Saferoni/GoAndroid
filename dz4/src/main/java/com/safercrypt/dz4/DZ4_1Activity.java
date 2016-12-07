3e529e23f13b513bb3ad7ea306002fdaa983b456
Saferoni <SafronovPN@gmail.com>
2016-12-08 00:57:32 +0200

    Сделана ДЗ  4_1

--- dz4/src/main/java/com/safercrypt/dz4/DZ4_1Activity.java
+++ dz4/src/main/java/com/safercrypt/dz4/DZ4_1Activity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -30,92 +31,53 @@ public class DZ4_1Activity extends AppCompatActivity{
         final TableLayout tableLayout = new TableLayout(this);
         tableLayout.setLayoutParams(new TableLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
+                ViewGroup.LayoutParams.WRAP_CONTENT
         ));
-        tableLayout.setStretchAllColumns(true);
 
-        // мотод для наполнения TableRow кнопками
+        TableRow tableRow1 = addTableRowWichButton(5);
+        TableRow tableRow2 = addTableRowWichButton(5);
+        TableRow tableRow3 = addTableRowWichButton(5);
+        TableRow tableRow4 = addTableRowWichButton(5);
+        TableRow tableRow5 = addTableRowWichButton(5);
 
-        TableRow tableRow1 = new TableRow(this);
-        TableRow tableRow2 = new TableRow(this);
-        TableRow tableRow3 = new TableRow(this);
-        TableRow tableRow4 = new TableRow(this);
-        TableRow tableRow5 = new TableRow(this);
-
-        // первый ряд кнопок
-        Button button1 = new Button(this);
-        buttonSet(button1);
-        Button button2 = new Button(this);
-        buttonSet(button2);
-        Button button3 = new Button(this);
-        buttonSet(button3);
-        Button button4 = new Button(this);
-        buttonSet(button4);
-        Button button5 = new Button(this);
-        buttonSet(button5);
-
-        tableRow1.addView(button1);
-        tableRow1.addView(button2);
-        tableRow1.addView(button3);
-        tableRow1.addView(button4);
-        tableRow1.addView(button5);
-
-        // второй ряд кнопок
-        Button button6 = new Button(this);
-        buttonSet(button6);
-        Button button7 = new Button(this);
-        buttonSet(button7);
-        Button button8 = new Button(this);
-        buttonSet(button8);
-        Button button9 = new Button(this);
-        buttonSet(button9);
-        Button button10 = new Button(this);
-        buttonSet(button10);
-
-        tableRow2.addView(button6);
-        tableRow2.addView(button7);
-        tableRow2.addView(button8);
-        tableRow2.addView(button9);
-        tableRow2.addView(button10);
-
-
-        // ну и так далее что их все писать надо подумать на оптимизацией
-        Button button11 = new Button(this);
-        Button button12 = new Button(this);
-        Button button13 = new Button(this);
-        Button button14 = new Button(this);
-        Button button15 = new Button(this);
-
-        Button button16 = new Button(this);
-        Button button17 = new Button(this);
-        Button button18 = new Button(this);
-        Button button19 = new Button(this);
-        Button button20 = new Button(this);
-
-        Button button21 = new Button(this);
-        Button button22 = new Button(this);
-        Button button23 = new Button(this);
-        Button button24 = new Button(this);
-        Button button25 = new Button(this);
 
         tableLayout.addView(tableRow1);
         tableLayout.addView(tableRow2);
+        tableLayout.addView(tableRow3);
+        tableLayout.addView(tableRow4);
+        tableLayout.addView(tableRow5);
         setContentView(tableLayout);
 
     }
-// метод настройки кнопки
-    public void buttonSet(final Button button){
+    // метод заполнения TableRow
+    public TableRow addTableRowWichButton(int value){
+        TableRow tableRaw = new TableRow(this);
+        for (int i = 0; i < value; i++){
+            tableRaw.addView(buttonSet(new Button(this)));
+        }
+        return tableRaw;
+    }
+
+    // метод настройки кнопки
+    public Button buttonSet(final Button button){
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        buttonParams.weight = 1;  //не срабатывает
+        //button.setLayoutParams(buttonParams); // почему тоданные параметры не срабатывают
         button.setText(generateRandomString(5));
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 button.setText(generateRandomString(5));
             }
         });
+    return button;
     }
 
-// метод геренрации случайной строки
+    // метод геренрации случайной строки
     public static String generateRandomString(int size) {
-        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String str = "abcdefghijklmnopqrstuvwxyz";
         Random RANDOM = new SecureRandom();
         String randomString = "";
         for (int i = 0; i < size; i++) {
