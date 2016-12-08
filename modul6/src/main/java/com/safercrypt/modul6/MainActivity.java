d49ce31ad60e155f6ea1c326166a4718b13bbbaf
Saferoni <SafronovPN@gmail.com>
2016-12-08 21:57:27 +0200

    Modul 6

---
+++ modul6/src/main/java/com/safercrypt/modul6/MainActivity.java
@@ -0,0 +1,64 @@
+package com.safercrypt.modul6;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2,button3, button4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Test.class));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse("http://google.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+
+}
---
+++ modul6/src/main/java/com/safercrypt/modul6/Test.java
@@ -0,0 +1,84 @@
+package com.safercrypt.modul6;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Created by pavelsafronov on 08.12.16.
+ */
+
+public class Test extends AppCompatActivity{
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //Создам и лайаут из программы а фигли
+            final TableLayout tableLayout = new TableLayout(this);
+            tableLayout.setLayoutParams(new TableLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            ));
+
+            TableRow tableRow1 = addTableRowWichButton(5);
+            TableRow tableRow2 = addTableRowWichButton(5);
+            TableRow tableRow3 = addTableRowWichButton(5);
+            TableRow tableRow4 = addTableRowWichButton(5);
+            TableRow tableRow5 = addTableRowWichButton(5);
+
+
+            tableLayout.addView(tableRow1);
+            tableLayout.addView(tableRow2);
+            tableLayout.addView(tableRow3);
+            tableLayout.addView(tableRow4);
+            tableLayout.addView(tableRow5);
+            setContentView(tableLayout);
+
+        }
+        // метод заполнения TableRow
+        public TableRow addTableRowWichButton(int value){
+            TableRow tableRaw = new TableRow(this);
+            for (int i = 0; i < value; i++){
+                tableRaw.addView(buttonSet(new Button(this)));
+            }
+            return tableRaw;
+        }
+
+        // метод настройки кнопки
+        public Button buttonSet(final Button button){
+            LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+            );
+            buttonParams.weight = 1;  //не срабатывает
+            //button.setLayoutParams(buttonParams); // почему тоданные параметры не срабатывают
+            button.setText(generateRandomString(5));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    button.setText(generateRandomString(5));
+                }
+            });
+            return button;
+        }
+
+        // метод геренрации случайной строки
+        public static String generateRandomString(int size) {
+            String str = "abcdefghijklmnopqrstuvwxyz";
+            Random RANDOM = new SecureRandom();
+            String randomString = "";
+            for (int i = 0; i < size; i++) {
+                int index = (int) (RANDOM.nextDouble() * str.length());
+                randomString += str.substring(index, index + 1);
+            }
+            return randomString;
+        }
+    }
